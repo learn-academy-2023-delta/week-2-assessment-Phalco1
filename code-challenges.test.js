a0e88d587cab4e177028feaf1d79a898ce87625b
Phalco1 <elphalco1@gmail.com>
2023-05-22 12:59:30 -0500

    It's not over yet...

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,31 +1,31 @@
-// ASSESSMENT 2: Coding Practical Questions with Jest
+// // ASSESSMENT 2: Coding Practical Questions with Jest
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+// // Use test driven development to complete the following questions
+// // Add appropriate dependencies: $ yarn add jest
 
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// // Reminder: The test will call your function
+// // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
+// // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
-// Pseudo code:
+// // Pseudo code:
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -34,9 +34,37 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// // Pseudo code:
+
+// //  1.) Write a describe statement to take in the argument of a string and function.
+
+// // 2.) Write an it statement in the describe block as a string describing what the test is doing with a function (anonymous function)
+
+// // 3.) Write 3 expect statement to take in the each encased function call with the .toEqual() method with the strings encased in the .toEqual method.
+
+// // 4.) Create a function with a conditional statement 
+
+// // 1.) CREATE
+
+describe("divideBy3", () => {
+    it("decides if the number inside is divisible by 3.", () => {
+        expect(divideBy3(object1.number)).toEqual("15 is divisible by three")
+        expect(divideBy3(object2.number)).toEqual("0 is divisible by three")
+        expect(divideBy3(object3.number)).toEqual("-7 is not divisible by three")
+    })
+})
+
+const divideBy3 = (object) => {
+    if (object % 3 === 0) {
+    return `${object} is divisible by three`
+    } else {
+    return `${object} is not divisible by three`
+    }
+}
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -49,7 +77,16 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// Pseudo code: 
+
+// array = randomNouns1
+
+// const arrayWords = (array) => {
+    
+// }
+
+// console.log(arrayWords(randomNouns1).join(" ").toUpperCase)
+// console.log(arrayWords(randomNouns2).join(" ").toUpperCase)
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
